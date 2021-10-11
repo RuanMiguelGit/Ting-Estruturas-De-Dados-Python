950d3e82c61de5d94e988034606a6aed5960e86a
Ruan Miguel <ruanmiguelps@gmail.com>
2021-10-11 19:48:08 -0300

    req 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -5,16 +5,18 @@ from ting_file_management.file_management import txt_importer
 def process(path_file, instance):
     """Aqui irá sua implementação"""
     instance.enqueue(path_file)
-    return print(
-        f"'nome_do_arquivo': '{path_file}'\n",
-        f"'qtd_linhas': {len(txt_importer(path_file))}\n",
-        f"'linhas_do_arquivo': {txt_importer(path_file)}\n",
-        file=sys.stdout,
-    )
+    sys.stdout.write(f"'nome_do_arquivo': '{path_file}'\n"),
+    sys.stdout.write(f"'qtd_linhas': {len(txt_importer(path_file))}\n"),
+    sys.stdout.write(f"'linhas_do_arquivo': {txt_importer(path_file)}\n"),
 
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    if len(instance) != 0:
+        removed = instance.dequeue()
+        sys.stdout.write(f"Arquivo {removed} removido com sucesso\n")
+    else:
+        sys.stdout.write("Não há elementos\n")
 
 
 def file_metadata(instance, position):
