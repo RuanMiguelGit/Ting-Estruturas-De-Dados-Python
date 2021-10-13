1924b1178763628b656d7a4469b5881f53261275
Ruan Miguel <ruanmiguelps@gmail.com>
2021-10-13 09:14:08 -0300

    req 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,6 +4,7 @@ from ting_file_management.file_management import txt_importer
 
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+
     instance.enqueue(path_file)
     sys.stdout.write(f"'nome_do_arquivo': '{path_file}'\n"),
     sys.stdout.write(f"'qtd_linhas': {len(txt_importer(path_file))}\n"),
